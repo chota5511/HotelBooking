dca37375c4cd47b7bd0ad46380fb7e4ea10ea4f6
chota5511 <binh_xu_2010@yahoo.com.sg>
2018-08-06 11:43:19 +0700

    0.1.0
    --Add Admin Login

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HotelBooking.Models;
 
 namespace HotelBooking.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin/Admin
-        public ActionResult Index()
+        HOTELEntities db = new HOTELEntities();
+        public ActionResult Login()
         {
             return View();
+        }
+        // GET: Admin/Admin
+        public ActionResult Index(string id,string password)
+        {
+            foreach (administrator a in db.administrators)
+            {
+                if (id == a.administratorid.Replace(" ",string.Empty))
+                {
+                    if (password == a.administratorpassword.Replace(" ",string.Empty))
+                    {
+                        return View();
+                    }
+                }
+            }
+            return RedirectToAction("Login","Admin");
         }
     }
 }
