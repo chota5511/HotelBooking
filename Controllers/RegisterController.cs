c7d87391ad5c3e86d857642c7d143569d4a7cd35
vankietpro854 <40312014+vankietpro854@users.noreply.github.com>
2018-08-09 16:01:38 +0700

    0.2.1 -  built 3
    -- Fix

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HotelBooking.Models;
+
+namespace HotelBooking.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Register(string userid, string username, DateTime? userbirth, string MaleOrFemale, string userpassword, string confirmpassword)
+        {
+            HOTELEntities db = new HOTELEntities();
+            user user = new user();
+            user.userid = userid;
+            user.username = username;
+            user.userbirth = userbirth;
+            user.usersex = MaleOrFemale;
+          
+            db.users.Add(user);
+            db.SaveChanges();
+            return View();
+        }
+    } 
+}
