74612e60925835dabbee999398a35b615011b8c2
chota5511 <binh_xu_2010@yahoo.com.sg>
2018-08-13 23:42:46 +0700

    0.2.8
    --Finish Register (fully function)

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -10,14 +10,38 @@ namespace HotelBooking.Controllers
     public class RegisterController : Controller
     {
         // GET: Register
-        public ActionResult Index()
+        public ActionResult Register()
         {
             return View();
         }
 
-        public ActionResult Register()
+        public ActionResult Update(string id, string name, DateTime birth, string sex, string password, string rpassword)
         {
-            return View();
+            HOTELEntities db = new HOTELEntities();
+            if (id != "" && password != "" && rpassword == password)
+            {
+                bool isHaved = false;
+                foreach(user u in db.users)
+                {
+                    if(u.userid.Replace(" ", "") == id)
+                    {
+                        isHaved = true;
+                        break;
+                    }
+                }
+                if(isHaved == false)
+                {
+                    user tmp = new user();
+                    tmp.userid = id;
+                    tmp.username = name;
+                    tmp.userpassword = password;
+                    tmp.userbirth = birth;
+                    tmp.usersex = sex;
+                    db.users.Add(tmp);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index", "Home");
         }
     } 
 }
