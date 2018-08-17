4b3115e9f1ec5e7c370768efc45d41ea4639904e
chota5511 <binh_xu_2010@yahoo.com.sg>
2018-08-17 17:51:59 +0700

    0.2.10
    --Add Booking page in main page
    --Change color from pink to blue in main page

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
